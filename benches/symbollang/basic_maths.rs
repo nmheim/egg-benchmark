9f0431a3486f317cb67057c1317979139f4f4f56
a <a@a.a>
2024-04-11 17:38:20 +0200

    make benchmarks equal

--- benches/symbollang/basic_maths.rs
+++ benches/symbollang/basic_maths.rs
@@ -1,5 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use egg::*;
+use egg_benchmark::simplify;
 
 define_language! {
     pub enum BasicMath {
@@ -65,40 +66,23 @@ pub fn basic_maths_rules() -> Vec<Rewrite<SymbolLang, ()>> {
 }
 
 
-
-/// parse an expression, simplify it using egg, and pretty print it back out
-pub fn simplify(s: &str) -> String {
-    // parse the expression, the type annotation tells it which Language to use
-    let expr: RecExpr<SymbolLang> = s.parse().unwrap();
-
-    // simplify the expression using a Runner, which creates an e-graph with
-    // the given expression and runs the given rules over it
-    let runner = Runner::default().with_expr(&expr).run(&basic_maths_rules());
-
-    // the Runner knows which e-class the expression given with `with_expr` is in
-    let root = runner.roots[0];
-
-    // use an Extractor to pick the best element of the root eclass
-    let extractor = Extractor::new(&runner.egraph, AstSize);
-    let (_, best) = extractor.find_best(root);
-    best.to_string()
-}
-
-
 pub fn basic_maths_benchmark(c: &mut Criterion) {
+    let rules = basic_maths_rules();
+    let expr = "(+ a (+ b (+ (* 0 c) d)))".parse().unwrap();
     c.bench_function(
         "basic_maths/simpl1",
         |b| b.iter(|| {
-            simplify(black_box("(+ a (+ b (+ (* 0 c) d)))"));
+            simplify(black_box(&expr), black_box(&rules), 8);
             //assert_eq!(result, "(+ d (+ b a))");
         })
     );
 
+    let expr = "(+ (+ (+ 0 (* (* 1 foo) 0)) (* a 0)) a)".parse().unwrap();
     c.bench_function(
         "basic_maths/simpl2",
         |b| b.iter(|| {
-            let result =  simplify(black_box("(+ (+ (+ 0 (* (* 1 foo) 0)) (* a 0)) a)"));
-            assert_eq!(result, "a");
+            let result =  simplify(black_box(&expr), black_box(&rules), 8);
+            assert_eq!(result, "a".parse().unwrap());
         })
     );
 }
